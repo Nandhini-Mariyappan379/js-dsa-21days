e99196bb49edbf58f28f62a14cc6f92f992af75b
Nandhini-Mariyappan379 <nandhinimariyappan379@gmail.com>
2025-07-11 17:32:01 +0530

    Update reverseString.js

--- Day01_Basics/reverseString.js
+++ Day01_Basics/reverseString.js
@@ -1,1 +1,23 @@
 
+// Problem: Write a function reverseString(str) that returns the reversed string.
+
+function reverseString(str) {
+    //Old Method
+    let result = []
+    let x = str.split('')
+    for (let i = x.length - 1; i >= 0; i--) {
+        result.push(x[i])
+    }
+    //Built-in Method
+    let reverseBuiltMethod = str.split('').reverse().join('')
+    return {
+        old: result.join(''),
+        new: reverseBuiltMethod
+    }
+}
+
+
+console.log(reverseString('NANDHINI'));
+console.log(reverseString('Hello worlds'));
+
+
